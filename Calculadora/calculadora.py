15c884198c2088544e12245a331c4b9b9126a41c
JulioSandoval1029384756 <135393015+JulioSandoval1029384756@users.noreply.github.com>
2025-08-01 15:23:05 -0600

    segundo commit

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,4 +1,19 @@
 def suma(a, b):
     return a + b
 
-print("Resultado:", suma(2, 3))
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "No se puede dividir entre cero"
+
+print("Suma:", suma(2, 3))
+print("Resta:", resta(5, 2))
+print("Multiplicación:", multiplicacion(4, 6))
+print("División:", division(10, 2))
